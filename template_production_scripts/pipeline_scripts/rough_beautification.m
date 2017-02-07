a17274ef2e3d915ba1bc8097e5f53b57b88adeae
Cristian Goina <goinac@janelia.hhmi.org>
2017-02-07 15:29:48 -0500

    rough and fine beautification scripts

---
+++ template_production_scripts/pipeline_scripts/rough_beautification.m
@@ -0,0 +1,63 @@
+% configure source
+rcsource.stack          = 'v12_acquire_merged';
+rcsource.owner          ='flyTEM';
+rcsource.project        = 'FAFB00';
+rcsource.service_host   = '10.40.3.162:8080';
+rcsource.baseURL        = ['http://' rcsource.service_host '/render-ws/v1'];
+rcsource.verbose        = 1;
+
+nfirst = 4435;
+nlast = 4446;
+
+% configure rough
+rcmontage.stack          = ['Revised_slab_' num2str(nfirst) '_' num2str(nlast) '_montage'];
+rcmontage.owner          ='flyTEM';
+rcmontage.project        = 'FAFB00_beautification';
+rcmontage.service_host   = '10.40.3.162:8080';
+rcmontage.baseURL        = ['http://' rcmontage.service_host '/render-ws/v1'];
+rcmontage.verbose        = 1;
+
+% configure rough
+rcrough.stack          = ['Revised_slab_' num2str(nfirst) '_' num2str(nlast) '_rough'];
+rcrough.owner          ='flyTEM';
+rcrough.project        = 'FAFB00_beautification';
+rcrough.service_host   = '10.40.3.162:8080';
+rcrough.baseURL        = ['http://' rcrough.service_host '/render-ws/v1'];
+rcrough.verbose        = 1;
+
+dir_rough_intermediate_store = '/nrs/flyTEM/khairy/FAFB00v13/montage_scape_pms';% intermediate storage of files
+dir_store_rough_slab = '/nrs/flyTEM/khairy/FAFB00v13/matlab_slab_rough_aligned';
+scale  = 0.02;
+
+finescale = 0.4;
+nbrs = 3;
+point_pair_thresh    = 5;
+
+%% generate rough alignment
+% configure montage-scape point-match generation
+ms.service_host                 = rcmontage.service_host;
+ms.owner                        = rcmontage.owner;
+ms.project                      = rcmontage.project;
+ms.stack                        = rcmontage.stack;
+ms.fd_size                      = '10'; % '8'
+ms.min_sift_scale               = '0.2';%'0.55';
+ms.max_sift_scale               = '1.0';
+ms.steps                        = '3';
+ms.similarity_range             = '15';
+ms.skip_similarity_matrix       = 'y';
+ms.skip_aligned_image_generation= 'y';
+ms.base_output_dir              = '/nrs/flyTEM/spark_montage/beautification';
+ms.script                       = '/groups/flyTEM/home/khairyk/EM_aligner/renderer_api/generate_montage_scape_point_matches.sh';%'../unit_tests/generate_montage_scape_point_matches_stub.sh'; %
+ms.number_of_spark_nodes        = '2.0';
+ms.first                        = num2str(nfirst);
+ms.last                         = num2str(nlast);
+ms.scale                        = num2str(scale);
+ms.run_dir                      = ['Slab_' ms.first '_' ms.last '_scale_' ms.scale];
+
+[L2, needs_correction, pmfn, zsetd, zrange, t, dir_spark_work, cmd_str, fn_ids, ...
+    target_solver_path, target_ids, target_matches, target_layer_images] =  ...
+    ...
+    solve_rough_slab(dir_store_rough_slab, rcmontage, ...
+    rcmontage, rcrough, ms, nfirst,...
+    nlast, dir_rough_intermediate_store, ...
+    1);
---
+++ template_production_scripts/pipeline_scripts/solve_beautification.m
@@ -0,0 +1,80 @@
+% fine-align the slab
+
+nfirst = 4435;
+nlast = 4446;
+
+% configure rough
+rcrough.stack          = ['Revised_slab_' num2str(nfirst) '_' num2str(nlast) '_rough'];
+rcrough.owner          ='flyTEM';
+rcrough.project        = 'FAFB00_beautification';
+rcrough.service_host   = '10.40.3.162:8080';
+rcrough.baseURL        = ['http://' rcrough.service_host '/render-ws/v1'];
+rcrough.verbose        = 1;
+
+% configure fine alignment
+rcfine.stack          = ['Revised_slab_' num2str(nfirst) '_' num2str(nlast) '_fine'];
+rcfine.owner          ='flyTEM';
+rcfine.project        = 'FAFB00_beautification';
+rcfine.service_host   = '10.40.3.162:8080';
+rcfine.baseURL        = ['http://' rcrough.service_host '/render-ws/v1'];
+rcfine.verbose        = 1;
+
+
+pm1.server = 'http://10.40.3.162:8080/render-ws/v1';
+pm1.owner = 'flyTEM';
+pm1.match_collection = 'FAFB_pm_2';
+
+pm2.server = 'http://10.40.3.162:8080/render-ws/v1';
+pm2.owner = 'flyTEM';
+pm2.match_collection = 'Beautification_cross_sift_00';
+
+pms = [pm1 pm2];
+
+%% generate list of potential tile pairs
+% chunck it up into pieces
+tp = {};
+tile_pairs = tile;
+chnk = 1000;
+tpcount = 1;
+count = 1;
+
+%% solve
+% configure solver
+opts.min_tiles = 20; % minimum number of tiles that constitute a cluster to be solved. Below this, no modification happens
+opts.degree = 1;    % 1 = affine, 2 = second order polynomial, maximum is 3
+opts.outlier_lambda = 1e2;  % large numbers result in fewer tiles excluded
+opts.solver = 'backslash';%'pastix';%%'gmres';%'backslash';'pastix';
+
+opts.pastix.ncpus = 8;
+opts.pastix.parms_fn = '/nrs/flyTEM/khairy/FAFB00v13/matlab_production_scripts/params_file.txt';
+opts.pastix.split = 1; % set to either 0 (no split) or 1
+
+opts.matrix_only = 0;   % 0 = solve , 1 = only generate the matrix
+opts.distribute_A = 1;  % # shards of A
+opts.dir_scratch = '/scratch/goinac';
+
+opts.min_points = 10;
+opts.max_points = 100;
+opts.nbrs = 3;
+opts.xs_weight = 15.0;
+opts.stvec_flag = 1;   % 0 = regularization against rigid model (i.e.; starting value is not supplied by rc)
+opts.distributed = 0;
+
+opts.lambda = 10.^(-1);
+opts.edge_lambda = 10^(-1);
+opts.A = [];
+opts.b = [];
+opts.W = [];
+
+% % configure point-match filter
+opts.filter_point_matches = 1;
+opts.pmopts.NumRandomSamplingsMethod = 'Desired confidence';
+opts.pmopts.MaximumRandomSamples = 5000;
+opts.pmopts.DesiredConfidence = 99.9;
+opts.pmopts.PixelDistanceThreshold = .01;
+
+opts.verbose = 1;
+opts.debug = 0;
+
+[mL, err] = system_solve(nfirst, nlast, rcrough, pms, opts, rcfine);
+disp(err);
